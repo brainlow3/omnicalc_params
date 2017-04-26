2a6c2f13907ed75d409311717371093ffb583538
Brian Lowell <brian.lowell@gmail.com>
2017-04-26 10:06:01 -0500

    new attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get("flexible/square/new", {:controller => "calculations", :action => "square_form"})
-  get("flexible/square/:number", {:controller => "calculations", :action => "square"})
-  get("/square_root/:number", {:controller => "calculations", :action => "flexible_square_root"})
-  get("/payment/:basis_points/:number_of_years/:present_value", {:controller => "calculations", :action => "flexible_payment"})
-
+  get("/square/new", {:controller => "calculations", :action => "square_form"})
+  get("/square/results", {:controller => "calculations", :action => "square"})
 end
